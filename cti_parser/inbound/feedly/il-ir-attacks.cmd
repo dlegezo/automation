26e7d89dd726b629c225b04abc28ce9bdec5211d
dlegezo <dlegezo@gmail.com>
2026-04-09 13:33:46 +0100

    refactor: comment out outdated cyber-attacks API call and add new TTPs JSON file

--- cti_parser/inbound/feedly/il-ir-attacks.cmd
+++ cti_parser/inbound/feedly/il-ir-attacks.cmd
@@ -1,3 +1,9 @@
-curl -X POST "https://api.feedly.com/v3/ml/relationships/cyber-attacks/dashboard/table" ^
+@REM curl -X POST "https://api.feedly.com/v3/ml/relationships/cyber-attacks/dashboard/table" ^
+@REM   -H "Authorization: Bearer %FEEDLY_CTI%" ^
+@REM   -H "Content-Type: application/json" ^
+@REM   --data "@il-ir-attacks.json"
+
+curl -X POST "https://api.feedly.com/v3/ml/ttps" ^
   -H "Authorization: Bearer %FEEDLY_CTI%" ^
-  -H "Content-Type: application/json" --data "@il-ir-attacks.json"
+  -H "Content-Type: application/json" ^
+  --data "@il-ir-ttps.json"
